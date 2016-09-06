6079c89c9389a388d25b7e29981a3f6508a0be7e
cunhatg <cunhatg@miamioh.edu>
2016-09-06 16:27:01 -0300

    Full implementation of momentjs lib

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,56 +1,17 @@
-var daysLeft = 0;
-var finalDate = {
-	month: 11,
-	day: 21,
-	hours: 7,
-	minutes: 50,
-	seconds: 0
-}
-
+// Moment Time Zone Add
 moment.tz.add("America/Kentucky/Louisville|CST CDT CWT CPT EST EDT|60 50 50 50 50 40|0101010102301010101010101010101010101454545454545414545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454|-261s0 1nX0 11B0 1nX0 3Fd0 Nb0 LPd0 11z0 RB0 8x30 iw0 Bb0 10N0 2bB0 8in0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 xz0 gso0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1VA0 LA0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0");
 moment.tz.add("America/Sao_Paulo|LMT BRT BRST|36.s 30 20|012121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212|-2glwR.w HdKR.w 1cc0 1e10 1bX0 Ezd0 So0 1vA0 Mn0 1BB0 ML0 1BB0 zX0 pTd0 PX0 2ep0 nz0 1C10 zX0 1C10 LX0 1C10 Mn0 H210 Rb0 1tB0 IL0 1Fd0 FX0 1EN0 FX0 1HB0 Lz0 1EN0 Lz0 1C10 IL0 1HB0 Db0 1HB0 On0 1zd0 On0 1zd0 Lz0 1zd0 Rb0 1wN0 Wn0 1tB0 Rb0 1tB0 WL0 1tB0 Rb0 1zd0 On0 1HB0 FX0 1C10 Lz0 1Ip0 HX0 1zd0 On0 1HB0 IL0 1wp0 On0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 Rb0 1zd0 Lz0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 On0 1zd0 On0 1C10 Lz0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 Rb0 1wp0 On0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 On0 1zd0 On0 1C10 Lz0 1C10 Lz0 1C10 Lz0 1C10 On0 1zd0 Rb0 1wp0 On0 1C10 Lz0 1C10 On0 1zd0|20e6");
 
-var final = moment(new Date(2016, 11, 21, 7, 50));
+// Date she arrives
+var finalDate = moment(new Date(2016, 11, 21, 7, 50));
 
 var timer = setInterval(myTimer, 1000);
 	function myTimer() {
-		var date = new Date();
-
+		var currentDate  = moment();
 		var saoPauloTime = moment().tz("America/Sao_Paulo");
 		var louisTime    = moment().tz("America/Kentucky/Louisville");
 
-		var currentDate = {
-			month: date.getMonth(),
-		 	day: date.getDate(),
-		 	hours: date.getHours(),
-		 	minutes: date.getMinutes(),
-		 	seconds: date.getSeconds()
-		}
-		
-		switch (currentDate.month) {
-			case 7:
-				daysLeft = 31 - currentDate.day;
-				daysLeft += 90;
-				break;
-			case 8:
-				daysLeft = 30 - currentDate.day;
-				daysLeft += 60;
-				break;
-			case 9:
-				daysLeft = 31 - currentDate.day;
-				daysLeft += 29;
-				break;
-			case 10:
-				daysLeft = 30 - currentDate.day;
-				break;
-			case 11:
-				daysLeft = finalDate.day - currentDate.day - 21;
-				break;	
-		}
-		daysLeft += 21;
-
-
-		document.getElementById("RJ").innerHTML = saoPauloTime.format("HH:mm:ss");
-		document.getElementById("OH").innerHTML = louisTime.format("HH:mm:ss");
-		document.getElementById("timer").innerHTML = daysLeft;
+		document.getElementById("RJ").innerHTML    = saoPauloTime.format("HH:mm:ss");
+		document.getElementById("OH").innerHTML    = louisTime.format("HH:mm:ss");
+		document.getElementById("timer").innerHTML = finalDate.diff(currentDate, "days");
 	}
